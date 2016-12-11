5d25f1b062731c2b5017408428221fbe29cd02b1
Clacson <cookiellacson@gmail.com>
2016-12-11 14:15:16 +0000

    regions by population chart completed, population button started

--- public/app.js
+++ public/app.js
@@ -18,4 +18,36 @@ var requestComplete = function(){
   var countries = JSON.parse(jsonString);
   console.log("request complete");
   console.log("countries", countries);
+  populateChart(countries);
 };
+
+var populateChart = function(countries){
+  var container = document.getElementById('pop-chart');
+  var title = "Regions By Population";
+  var series = getRegionInfo(countries);
+  categories = ["population"];
+  new PopColumnChart(container, title, series, categories);
+};
+
+getRegionInfo = function(countries){
+  var populations = {};
+  countries.forEach(function(country){
+    if (populations[country.region]){
+      populations[country.region] += country.population;
+    } else {
+      populations[country.region] = country.population;
+    }
+  });
+  console.log(populations);
+  var series = [];
+  for (var region in populations){
+    if (region) {
+      console.log("Region: ", region, "Pop: ", populations[region]);
+      series.push({name: region, data: [populations[region]]});
+    }
+  }
+  return series;
+};
+
+
+
